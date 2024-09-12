8eef895c86884fe5e5890f18a4b6ce85c70dd5b4
weikaj <weikaj276@gmail.com>
2024-09-12 10:47:40 +0200

    Add kosugi options for soil hydraulic conductivity

--- src/calc_rsoil.m
+++ src/calc_rsoil.m
@@ -1,13 +1,27 @@
-function [PSIs, rsss, rrr, rxx] = calc_rsoil(Rl, ModelSettings, SoilVariables, VanGenuchten, bbx, GroundwaterSettings)
+function [PSIs, rsss, rrr, rxx] = calc_rsoil(Rl, ModelSettings, SoilVariables, VanGenuchten, bbx, GroundwaterSettings, Kosugi, options)
     % load Constants
     Constants = io.define_constants();
 
     SMC = SoilVariables.Theta_LL(1:end - 1, 2);
     Se  = (SMC - VanGenuchten.Theta_r') ./ (VanGenuchten.Theta_s' - VanGenuchten.Theta_r');
-    Ksoil = SoilVariables.Ks' .* Se.^Constants.l .* (1 - (1 - Se.^(1 ./ VanGenuchten.m')).^(VanGenuchten.m')).^2;
-
+    if strcmp(options.modelType, 'VanGenuchten')
+        Ksoil = SoilVariables.Ks' .* Se.^Constants.l .* (1 - (1 - Se.^(1 ./ VanGenuchten.m')).^(VanGenuchten.m')).^2;
+    elseif strcmp(options.modelType, 'Kosugi') 
+        
+        Ksoil = SoilVariables.Ks' .* (se^Constants.l) .* 0.5 * erfc((erfcinv(2 * Se) + Kosugi.sigma ./ sqrt(2))).^2;
+    else
+        error('Invalid model type. Choose either "VanGenuchten" or "Kosugi".');
+    end   
+        
     if ~GroundwaterSettings.GroundwaterCoupling % no Groundwater coupling
-        PSIs = -((Se.^(-1 ./ VanGenuchten.m') - 1).^(1 ./ VanGenuchten.n')) ./ (VanGenuchten.Alpha * 100)' .* bbx;
+        
+        if strcmp(options.modelType, 'VanGenuchten')
+            PSIs = -((Se.^(-1 ./ VanGenuchten.m') - 1).^(1 ./ VanGenuchten.n')) ./ (VanGenuchten.Alpha * 100)' .* bbx;
+        elseif strcmp(options.modelType, 'Kosugi')
+            PSIs = Kosugi.mu * exp(sqrt(2) * Kosugi.sigma * erfcinv(Se)) .* bbx;
+        else
+        error('Invalid model type. Choose either "VanGenuchten" or "Kosugi".');
+        end    
     else % Groundwater coupling is activated, added by Mostafa (see https://github.com/EcoExtreML/STEMMUS_SCOPE/issues/231)
         % Change PSIs with SoilVariables.hh to correct hydraulic head (matric potential + gravity) of the saturated layers
         for i = 1:ModelSettings.NL
