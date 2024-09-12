c3e29c1ee7f1c3c08ab27cb5ac4ed8147073dcce
weikaj <weikaj276@gmail.com>
2024-09-12 10:54:28 +0200

    Add kosugi parameters

--- src/+init/updateBtmh.m
+++ src/+init/updateBtmh.m
@@ -1,7 +1,7 @@
-function Btmh = updateBtmh(VanGenuchten, SoilVariables, i, ModelSettings)
+function Btmh = updateBtmh(VanGenuchten, SoilVariables, i, ModelSettings, Kosugi, options)
 
-    if ModelSettings.SWCC == 1   % VG soil water retention model
-        Btmh = init.calcInitH(VanGenuchten.Theta_s(i), VanGenuchten.Theta_r(i), SoilVariables.BtmX, VanGenuchten.n(i), VanGenuchten.m(i), VanGenuchten.Alpha(i));
+    if ModelSettings.SWCC == 1   %  VG and Kosugi soil water retention model
+        Btmh = init.calcInitH(VanGenuchten.Theta_s(i), VanGenuchten.Theta_r(i), SoilVariables.BtmX, VanGenuchten.n(i), VanGenuchten.m(i), VanGenuchten.Alpha(i),Kosugi.mu(i), Kosugi.sigma(i), options);
     else
         Btmh = SoilVariables.Phi_s(i) * (SoilVariables.BtmX / VanGenuchten.Theta_s(i))^(-1 / SoilVariables.Lamda(i));
     end
