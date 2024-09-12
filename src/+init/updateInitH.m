0c7b5fa474beeccfef61e91201ae45e066e575df
weikaj <weikaj276@gmail.com>
2024-09-12 10:53:25 +0200

    Add Calculate soil water potential (initH) based on kosugi

--- src/+init/updateInitH.m
+++ src/+init/updateInitH.m
@@ -1,7 +1,7 @@
-function initH = updateInitH(initX, VanGenuchten, SoilVariables, i, ModelSettings)
+function initH = updateInitH(initX, VanGenuchten, SoilVariables, i, ModelSettings, Kosugi, options)
 
-    if ModelSettings.SWCC == 1   % VG soil water retention model
-        initH = init.calcInitH(VanGenuchten.Theta_s(i), VanGenuchten.Theta_r(i), initX, VanGenuchten.n(i), VanGenuchten.m(i), VanGenuchten.Alpha(i));
+    if ModelSettings.SWCC == 1   % VG and Kosugi soil water retention model
+        initH = init.calcInitH(VanGenuchten.Theta_s(i), VanGenuchten.Theta_r(i), initX, VanGenuchten.n(i), VanGenuchten.m(i), VanGenuchten.Alpha(i), Kosugi.mu(i), Kosugi.sigma(i), options);
     else
         initH = SoilVariables.Phi_s(i) * (InitX / VanGenuchten.Theta_s(i))^(-1 / SoilVariables.Lamda(i));
     end
