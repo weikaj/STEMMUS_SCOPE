c5d010fb7b6f63de24c0c5dc560333b0917c4407
weikaj <weikaj276@gmail.com>
2024-09-12 11:03:46 +0200

    Add kosugi equation

--- src/+conductivity/+hydraulicConductivity/calculateTheta_m.m
+++ src/+conductivity/+hydraulicConductivity/calculateTheta_m.m
@@ -1,12 +1,20 @@
-function theta_m = calculateTheta_m(gamma_hh, VanGenuchten, POR)
+function theta_m = calculateTheta_m(gamma_hh, VanGenuchten, POR, Kosugi, options)
 
     theta_s = VanGenuchten.Theta_s;
     theta_r = VanGenuchten.Theta_r;
     alpha = VanGenuchten.Alpha;
     n = VanGenuchten.n;
     m = VanGenuchten.m;
-
-    theta_m = gamma_hh * theta_r + (theta_s - gamma_hh * theta_r) * (1 + abs(alpha * (-1))^n)^m;
+    mu = Kosugi.mu;
+    sigma = Kosugi.sigma;
+    
+    if strcmp(options.modelType, 'VanGenuchten')
+        theta_m = gamma_hh * theta_r + (theta_s - gamma_hh * theta_r) * (1 + abs(alpha * (-1))^n)^m;
+    elseif strcmp(options.modelType, 'Kosugi')
+        theta_m = gamma_hh * theta_r + 0.5 * (theta_s - gamma_hh *theta_r) * erfc(log(1 / mu) / sigma * sqrt(2));
+    else
+        error('Invalid model type. Choose either "VanGenuchten" or "Kosugi".');
+    end  
     if theta_m >= POR
         theta_m = POR;
     elseif theta_m <= theta_s
